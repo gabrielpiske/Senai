0f79cf792bc97cb3ba766231b07d5a2004f2372c
Gabriel Piske <156837794+gabrielpiske@users.noreply.github.com>
2024-04-24 15:33:54 -0300

    java

--- Programador de Sistemas da Informação/Lógica de Programação/Java/JogodaNave/src/jogodanave/JogodaNave.java
+++ Programador de Sistemas da Informação/Lógica de Programação/Java/JogodaNave/src/jogodanave/JogodaNave.java
@@ -1,21 +1,59 @@
 package jogodanave;
 
-import java.awt.event.ActionListener;
-import java.awt.event.KeyListener;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.*;
+import java.awt.Image;
+import java.awt.event.KeyEvent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
-public class ImagePanel extends JPanel implements ActionListener, KeyListener {
+class ImagePanel extends JPanel{
     
     private Image nave;
     private Image meteoro;
     private Image bala;
+    private int xNave;
+    private int yNave;
+    private int xMet;
+    private int yMet;
+    private int xBala;
+    private int yBala;
     
     public ImagePanel(){
-        addKeyListener(this);
+        
+        nave = new ImageIcon("src/jogodanave/nave_image.png").getImage();
+        meteoro = new ImageIcon("src/jogodanave/meteoro.png").getImage();
+        bala = new ImageIcon("src/jogodanave/tiro.png").getImage();
+        xNave = 200;
+        yNave = 200;
+        
         setFocusable(true);
         setFocusTraversalKeysEnabled(false);
+    }
+    
+    @Override
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        clearImage(g);
+        drawImage((Graphics2D) g, xNave, yNave);
+    }
+    
+    public void drawImage(Graphics2D g, int x, int y){
+        g.drawImage(nave, xNave, yNave, this);
+    }
+    
+    private void clearImage(Graphics g){
+        g.setColor(Color.white);
+        g.fillRect(0, 0, getWidth(), getHeight());
+    }
+    
+    @Override
+    public void keypressed(KeyEvent e){
+        int key = e.getKeyCode();
+        //if(key == KeyEvent.)
     }
 }
 
