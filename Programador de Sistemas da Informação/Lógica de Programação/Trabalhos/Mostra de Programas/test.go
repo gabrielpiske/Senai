83a4b44e48e65aebd77685a2e82896118fcad3ce
Gabriel Piske <156837794+gabrielpiske@users.noreply.github.com>
2024-05-10 16:39:25 -0300

    Create test.go

---
+++ Programador de Sistemas da Informação/Lógica de Programação/Trabalhos/Mostra de Programas/test.go
@@ -0,0 +1,18 @@
+package test
+
+import (
+	"fmt"
+)
+
+func main{
+	var num1 int = 4
+	num2 := 2
+
+	result := dividir(num1, num2)
+
+	fmt.Println(result)
+}
+
+func dividir(a, b int) int {
+	return a / b
+}
