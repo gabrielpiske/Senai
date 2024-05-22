87b19dbbcf21239051d8004e35f6fb285cc50390
Gabriel Piske <156837794+gabrielpiske@users.noreply.github.com>
2024-05-21 21:11:14 -0300

    Update Mensagem.java

--- Técnico em Desenvolvimentos de Sistemas/Lógica de Programação/Java/Mensagem/src/Mensagem.java
+++ Técnico em Desenvolvimentos de Sistemas/Lógica de Programação/Java/Mensagem/src/Mensagem.java
@@ -1,12 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 /**
  *
  * @author gabriel_piske
  */
 public class Mensagem {
+    public static void boasVindas(){
+        
+    }
     
+    public static void introducao(){
+        
+    }
+    
+    public static void atencao(){
+        
+    }
+    
+    public static void despedida(){
+        
+    }
 }
