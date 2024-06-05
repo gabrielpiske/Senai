bf637932f1840606b358fc9a16079449d5207f15
Gabriel Piske <156837794+gabrielpiske@users.noreply.github.com>
2024-06-05 19:35:02 -0300

    pilha

---
+++ Técnico em Desenvolvimentos de Sistemas/Lógica de Programação/Java/Pilha_VerificadorNumerico/src/pilha_verificadornumerico/Pilha_VerificadorNumerico.java
@@ -0,0 +1,14 @@
+package pilha_verificadornumerico;
+
+/**
+ *
+ * @author gabriel_piske
+ */
+public class Pilha_VerificadorNumerico {
+
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
