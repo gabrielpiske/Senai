8555bd1f3d04772bb65be651dc9d74f9edc9d035
Gabriel Piske <156837794+gabrielpiske@users.noreply.github.com>
2024-06-05 19:56:25 -0300

    verificadorNumerico

--- Técnico em Desenvolvimentos de Sistemas/Lógica de Programação/Java/Pilha_VerificadorNumerico/src/pilha_verificadornumerico/Pilha_VerificadorNumerico.java
+++ Técnico em Desenvolvimentos de Sistemas/Lógica de Programação/Java/Pilha_VerificadorNumerico/src/pilha_verificadornumerico/Pilha_VerificadorNumerico.java
@@ -1,4 +1,14 @@
+/*
+    Escreva um programa que leia 10 numeros. Para cada numero lido faça:
+    1- se o numero for par, empilhe na pilha chamada par
+    2- se o numero for impar, empilhe na pilha chamada impar
+    3- se o numero for "0", desempilhe um elemento de cada pilha.
+    4- caso alguma pilha esteja vazia, mostre uma mensagem de erro.
+ */
 package pilha_verificadornumerico;
+
+import java.util.Scanner;
+import java.util.Stack;
 
 /**
  *
@@ -6,9 +16,32 @@ package pilha_verificadornumerico;
  */
 public class Pilha_VerificadorNumerico {
 
-    
     public static void main(String[] args) {
-        
+        Scanner ler = new Scanner(System.in);
+        Stack<Integer> par = new Stack<>();
+        Stack<Integer> impar = new Stack<>();
+
+        System.out.println("Entre com 10 numeros: ");
+        for (int i = 0; i < 10; i++) {
+            int numero = ler.nextInt();
+
+            if (numero == 0) {
+                if (!par.isEmpty() && !impar.isEmpty()) {
+                    int desempilhadoPar = par.pop();
+                    int desempilhadoImpar = impar.pop();
+                    System.out.println("Par Desempilhado: " + desempilhadoPar);
+                    System.out.println("Impar Desempilhado: " + desempilhadoImpar);
+                } else {
+                    System.out.println("Erro: As pilhas estao vazias.");
+                }
+            } else if (numero % 2 == 0) {
+                par.push(numero);
+                System.out.println("Empilhado na pilha par: " + numero);
+            } else {
+                impar.push(numero);
+                System.out.println("Empilhado na pilha impar: " + numero);
+            }
+        }
     }
-    
+
 }
