aa102382b8933830c78deeccbd38a7194f8ca6d6
Gabriel Piske <156837794+gabrielpiske@users.noreply.github.com>
2024-06-05 21:25:27 -0300

    filaPriority

--- Técnico em Desenvolvimentos de Sistemas/Lógica de Programação/Java/Fila_Priority/src/fila_priority/Fila_Priority.java
+++ Técnico em Desenvolvimentos de Sistemas/Lógica de Programação/Java/Fila_Priority/src/fila_priority/Fila_Priority.java
@@ -1,4 +1,9 @@
+/*
+    Implemente uma fila de prioridade - Priority
+*/
 package fila_priority;
+
+import java.util.PriorityQueue;
 
 /**
  *
@@ -7,7 +12,24 @@ package fila_priority;
 public class Fila_Priority {
 
     public static void main(String[] args) {
+        PriorityQueue objFila = new PriorityQueue();
         
+        // Adicionando elementos na fila
+        objFila.add("Primeiro");
+        objFila.add("Segundo");
+        objFila.add("Terceiro");
+        
+        // Mostrando a fila
+        System.out.println("Elementos na fila: " + objFila);
+        
+        // Removendo
+        System.out.println("Elemento removido: " + objFila.poll());
+
+        // Mostrando
+        System.out.println("Elementos na fila após remoção: " + objFila);
+
+        // Mostrando o próximo elemento a ser removido sem removê-lo
+        System.out.println("Próximo elemento a ser removido: " + objFila.peek());
     }
     
 }
