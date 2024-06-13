11a49b80d8d2b6c429341673841709d4c8a4a89e
Gabriel Piske <156837794+gabrielpiske@users.noreply.github.com>
2024-06-13 16:45:43 -0300

    aprimorando o projeto do banco
    criando novos métodos, modularizando o código em funções e deixando-o funcional.

--- Técnico em Desenvolvimentos de Sistemas/Lógica de Programação/Java/AppBanco/src/appbanco/AppBanco.java
+++ Técnico em Desenvolvimentos de Sistemas/Lógica de Programação/Java/AppBanco/src/appbanco/AppBanco.java
@@ -1,13 +1,177 @@
 package appbanco;
 
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.util.Scanner;
+
 /**
  *
  * @author gabriel_piske
  */
 public class AppBanco {
-
+    
+    //Scanner Global
+    private static Scanner ler = new Scanner(System.in);
+    
     public static void main(String[] args) {
-        //Conta conta1 = new Conta();
+        Conta conta = new Conta("001");
+        boolean rodar = true;
+        
+        do{
+            menu();
+            int menu = ler.nextInt();
+            ler.nextLine(); //consumir linha nova
+            
+            //Entrada no sistema de decisão
+            switch (menu) {
+                case 1:
+                    criarConta(conta);
+                    break;
+                case 2:
+                    verStatus(conta);
+                    break;
+                case 3:
+                    verSaldo(conta);
+                    break;
+                case 4:
+                    depositar(conta);
+                    break;
+                case 5:
+                    sacar(conta);
+                    break;
+                case 6:
+                    transferir(conta);
+                    break;
+                case 7:
+                    aplicarJuros(conta);
+                    break;
+                case 8:
+                    extrato(conta);
+                    break;
+                case 9:
+                    closeConta(conta);
+                    rodar = false;
+                    break;
+                case 10:
+                    rodar = false;
+                    break;
+                default:
+                    System.err.println("Entrada Invalida. Tente Novamente");
+            }
+        }while(rodar);
     }
     
+    //Desenho do Menu Inicial
+    private static void menu(){
+        clearConsole();
+        System.out.println("\n===============================================================");
+        System.out.println("=               Aplicativo Gerenciador de Contas              =");
+        System.out.println("=-------------------------------------------------------------=");
+        System.out.println("=                             MENU                            =");
+        System.out.println("=-------------------------------------------------------------=");
+        System.out.println("=   1 - Criar Conta                 2 - Ver Status da Conta   =");
+        System.out.println("=   3 - Ver Saldo                   4 - Depositar             =");
+        System.out.println("=   5 - Sacar                       6 - Transferir            =");
+        System.out.println("=   7 - Aplicar Juros (Poupanca)    8 - Exibir Extrato        =");
+        System.out.println("=   9 - Fechar Conta                10 - Sair                 =");
+        System.out.println("===============================================================");
+        System.out.print("Entrada: ");
+    }
+    
+    //Limpa Console
+    public final static void clearConsole() {
+        try {
+            Robot robot = new Robot();
+            robot.setAutoDelay(10);
+            robot.keyPress(KeyEvent.VK_CONTROL);
+            robot.keyPress(KeyEvent.VK_L);
+            robot.keyRelease(KeyEvent.VK_CONTROL);
+            robot.keyRelease(KeyEvent.VK_L);
+        } catch (AWTException ex) {
+        }
+    }
+    
+    //1 - Criar conta
+    private static void criarConta(Conta conta){
+        clearConsole();
+        //Entrada Tipo da Conta
+        System.out.println("Tipos de Conta: 0-Corrente  1-Salario   2-Poupanca");
+        System.out.print("Escolha o tipo de conta: ");
+        int tpConta = ler.nextInt();
+        ler.nextLine();
+        conta.setTipoConta(tpConta);
+        
+        //Entrada Numero da Conta
+        System.out.print("Entre com o numero da Conta: ");
+        int nmConta = ler.nextInt();
+        ler.nextLine();
+        conta.setNmConta(nmConta);
+        
+        //Entrada Nome o Titular
+        System.out.print("Entre com nome do titular: ");
+        String titular = ler.nextLine();
+        conta.setTitular(titular);
+        
+        clearConsole();
+        
+        System.out.println("Conta Criada com Sucesso!");
+    }
+    
+    //2 - Ver Status
+    private static void verStatus(Conta conta){
+        clearConsole();
+        conta.verStatus();
+    }
+    
+    //3- Ver Saldo
+    private static void verSaldo(Conta conta){
+        clearConsole();
+        System.out.println("Saldo Atual: R$" + conta.getSaldo());
+    }
+    
+    //4- Depositar
+    private static void depositar(Conta conta){
+        clearConsole();
+        System.out.print("Valor para depostito: R$");
+        float valor = ler.nextFloat();
+        ler.nextLine();
+        conta.depositar(valor);
+    }
+    
+    //5- Sacar
+    private static void sacar(Conta conta){
+        clearConsole();
+        System.out.print("Valor do Saque: R$");
+        float valor = ler.nextFloat();
+        ler.nextLine();
+        conta.sacar(valor);
+    }
+    
+    //6- Transferir
+    private static void transferir(Conta conta){
+        clearConsole();
+        System.out.print("Valor da Transferencia: R$");
+        float valor = ler.nextFloat();
+        ler.nextInt();
+        conta.transferir(valor);
+    }
+    
+    //7- Aplicar Juros
+    private static void aplicarJuros(Conta conta){
+        clearConsole();
+        conta.aplyJuros();
+    }
+    
+    //8- Extrato
+    private static void extrato(Conta conta){
+        clearConsole();
+        conta.exibirExtrato();
+    }
+    
+    //9- Fechar Conta
+    private static void closeConta(Conta conta){
+        clearConsole();
+        conta.fecharConta();
+    }
 }
--- Técnico em Desenvolvimentos de Sistemas/Lógica de Programação/Java/AppBanco/src/appbanco/Conta.java
+++ Técnico em Desenvolvimentos de Sistemas/Lógica de Programação/Java/AppBanco/src/appbanco/Conta.java
@@ -17,7 +17,8 @@ public class Conta {
     private float limite = 1500.0f;
     private float limiteMax = 1500.0f;
     private boolean ativo = true;
-    private Stack historico = new Stack();
+    private Stack<String> historico = new Stack<>();
+    
 
     //Construtor Base
     public Conta(String idBanco) {
@@ -26,7 +27,7 @@ public class Conta {
         this.saldo = 0.00f;
     }
 
-    //Gets e Setters
+    //Gets e Sets
     public int getTipoConta() {
         return tpConta;
     }
@@ -69,65 +70,105 @@ public class Conta {
     public void setAtivo(boolean ativo) {
         this.ativo = ativo;
     }
-    public Stack getHistorico() {
+    public Stack<String> getHistorico() {
         return historico;
     }
-    public void setHistorico(Stack historico) {
+    public void setHistorico(Stack<String> historico) {
         this.historico = historico;
     }
 
     //Métodos da classe
+    
+    //Realização de Saque e conferencia do limite
     public void sacar(float saque) {
         if (this.saldo >= saque) {
             this.saldo -= saque;
-        } else if (this.tpConta == 0 && this.saldo + this.limite >= saque) {
-            this.saldo = 0.0f;
-            this.limite -= saque - this.saldo;
+            historico.push("Saque de: R$" + saque);
             System.out.println("Saque realizado com sucesso");
+        } else if (this.tpConta == 0 && this.saldo + this.limite >= saque) {
+            this.limite -= (saque - this.saldo);
+            this.saldo = 0.0f;
+            historico.push("Saque de: R$" + saque + " (usando limite)");
+            System.out.println("Saque realizado com sucesso utilizando limite");
         } else {
             System.err.println("Voce nao tem saldo suficiente para este saque.");
         }
     }
-
+    
+    //Distribuição correta dos depositos entre limite e saldo total
     public void depositar(float dep) {
-        if (this.limiteMax >= this.limite) {
-            if (this.limite + dep > this.limiteMax) {
-                this.saldo += dep - (this.limiteMax - limite);
-                this.limite = 1500.0f;
-            } else {
+        if(this.limite < this.limiteMax){
+            float diferencaLimite = this.limiteMax - this.limite;
+            if(dep <= diferencaLimite){
                 this.limite += dep;
-            }
-        } else {
-            this.limite += dep;
-        }
-        System.out.println("Deposito de " + dep + " realizado");
-        System.out.println("Limite Atualizado: " + this.limite);
-    }
-
-    public void transferir(float transf) {
-        if(this.tpConta == 0){
-            if(transf > this.saldo + this.limite){
-                System.err.println("Valor em Conta Insuficiente");
-            } else if(transf <= this.saldo){
-                this.saldo -= transf;
-                System.out.println("Valor Transferido com Sucesso");
             } else{
-                this.limite -= transf - this.saldo;
-                this.saldo = 0.0f;
+                this.limite = this.limiteMax;
+                this.saldo += (dep - diferencaLimite);
             }
         } else{
-            if(transf >= this.saldo){
+            this.saldo += dep;
+        }
+        historico.push("Deposito de: R$" + dep);
+        System.out.println("Deposito de R$" + dep + " realizado");
+        System.out.println("Saldo Atualizado: R$" + this.saldo);
+        System.out.println("Limite Atualizado: R$" + this.limite);
+    }
+    
+    //Função de Transfêrencia com verificação de saldo e limite
+    public void transferir(float transf) {
+        if (this.tpConta == 0) {
+            if (transf > this.saldo + this.limite) {
                 System.err.println("Valor em Conta Insuficiente");
-            } else{
+            } else if (transf <= this.saldo) {
                 this.saldo -= transf;
+                System.out.println("Valor Transferido com Sucesso");
+            } else {
+                this.limite -= (transf - this.saldo);
+                this.saldo = 0.0f;
+            }
+        } else {
+            if (transf >= this.saldo) {
+                System.err.println("Valor em Conta Insuficiente");
+            } else {
+                this.saldo -= transf;
+                System.out.println("Valor Transferido com Sucesso");
             }
         }
+        historico.push("Transferencia de: R$" + transf);
     }
-
-    public float verSaldo() {
-        return this.saldo;
+    
+    //Aplicar porcentagem de juros ao saldo
+    public void aplyJuros(){
+        if(this.tpConta == 2){
+            this.saldo += this.saldo * 0.01f;
+            historico.push("Juros Aplicados");
+            System.out.println("Juros Aplicados com Sucesso");
+        } else {
+            System.err.println("Juros somente para contas Poupancas");
+        }
     }
-
+    
+    //Exibição do extrato
+    public void exibirExtrato(){
+        System.out.println("Extrato da Conta: ");
+        for(String evt : historico){
+            System.out.println(evt);
+        }
+    }
+    
+    //Fechamento de Conta
+    public void fecharConta(){
+        if(this.saldo == 0 && this.limite == this.limiteMax){
+            this.ativo = false;
+            historico.push("Conta Fechada");
+            System.out.println("Conta Fechada com Sucesso");
+        } else{
+            System.err.print("Saldo ou Limite pendente ");
+            System.out.println("A conta não pode ser fechada");
+        }
+    }
+    
+    //Exibe no console todos os status da conta
     public void verStatus() {
         System.out.println("|======= Informacoes da Conta  =======|");
         System.out.println("Tipo.............: " + this.tpConta);
@@ -135,7 +176,7 @@ public class Conta {
         System.out.println("Titular..........: " + this.titular);
         System.out.println("Saldo............: " + this.saldo);
         System.out.println("Limite...........: " + this.limite);
-        System.out.println("Status...........: " + this.ativo);
+        System.out.println("Status...........: " + (this.ativo ? "Ativo" : "Fechado"));
         System.out.println("|=====================================|");
     }
 }
