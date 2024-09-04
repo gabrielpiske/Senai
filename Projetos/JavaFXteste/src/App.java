62561b0d05ff74c983621ae7a055aa028aeb2ea3
Gabriel Piske <156837794+gabrielpiske@users.noreply.github.com>
2024-09-04 14:06:37 -0300

    teste javafx

---
+++ Projetos/JavaFXteste/src/App.java
@@ -0,0 +1,30 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class App extends Application{
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Teste JavaFX");
+        Button botao = new Button("Clique Aqui");
+        botao.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event){
+                System.out.println("Botão clicado!");
+            }
+        });
+
+        StackPane root = new StackPane();
+        root.getChildren().addAll(botao);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+    }
+}
