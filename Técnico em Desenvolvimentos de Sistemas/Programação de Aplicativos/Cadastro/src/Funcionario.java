830f5389d9b49b90588bc9457b4a512fde0e3bf1
Gabriel Piske <156837794+gabrielpiske@users.noreply.github.com>
2024-09-30 21:30:18 -0300

    aula gustavo

---
+++ Técnico em Desenvolvimentos de Sistemas/Programação de Aplicativos/Cadastro/src/Funcionario.java
@@ -0,0 +1,76 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
+public class Funcionario {
+    private String nome, cpf, endereco, telefone, cargo;
+    private ArrayList<Funcionario> listaFuncionarios;
+
+    public Funcionario(){
+    }
+
+    public Funcionario(String nome, String cpf, String endereco, String telefone, String cargo){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.endereco = endereco;
+        this.telefone = telefone;
+        this.cargo = cargo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String toString(){
+        return nome+";"+cpf+";"+endereco+";"+telefone+";"+cargo;
+    }
+
+    public void exportarParaTXT(ArrayList<Funcionario>listaFuncionarios){
+        try(BufferedWriter dados = new BufferedWriter(new FileWriter("Funcionarios.txt"))){
+            for(Funcionario f:listaFuncionarios){
+                dados.write(f.toString());
+                dados.newLine();
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro ao exportar o arquivo: " + e.getMessage());            
+        }
+    }
+}
---
+++ Técnico em Desenvolvimentos de Sistemas/Programação de Aplicativos/Cadastro/src/Main.java
@@ -0,0 +1,205 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFormattedTextField;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.text.MaskFormatter;
+
+public class Main extends JFrame {
+
+    private JButton btnCadastrar, btnConsultar, btnAtualizar, btnExcluir, btnCargo;
+    private JTable tabelaFuncionarios;
+    private DefaultTableModel modeloTabela;
+    private ArrayList<Funcionario> listaFuncionarios;
+
+    public Main() throws ParseException {
+        listaFuncionarios = new ArrayList<>();
+        setLayout(null);
+
+        // Criação dos JLabels
+        JLabel labelNome = new JLabel("Nome:");
+        JLabel labelCpf = new JLabel("CPF:");
+        JLabel labelEndereco = new JLabel("Endereço:");
+        JLabel labelTelefone = new JLabel("Telefone:");
+        JLabel labelCargo = new JLabel("Cargo:");
+
+        // Definição da posição dos JLabels
+        labelNome.setBounds(30, 30, 100, 30);
+        labelCpf.setBounds(30, 70, 100, 30);
+        labelEndereco.setBounds(30, 110, 100, 30);
+        labelTelefone.setBounds(30, 150, 100, 30);
+        labelCargo.setBounds(30, 190, 100, 30);
+
+        // Criação dos JTextFields
+        JTextField campoNome = new JTextField();
+        // JTextField campoCpf = new JTextField();
+        JTextField campoEndereco = new JTextField();
+        // JTextField campoTelefone = new JTextField();
+        JTextField campoCargo = new JTextField();
+
+        // Mascara para cpf
+        MaskFormatter formatarCpf = new MaskFormatter("###.###.###.##");
+        formatarCpf.setPlaceholderCharacter('_');
+        JFormattedTextField campoCpf = new JFormattedTextField(formatarCpf);
+        campoCpf.setColumns(14);
+
+        // Mascara para Telefone
+        MaskFormatter formatarFone = new MaskFormatter("(##) #####-####");
+        formatarFone.setPlaceholderCharacter('_');
+        JFormattedTextField campotelefone = new JFormattedTextField(formatarFone);
+        campotelefone.setColumns(14);
+
+        // Definição da posição dos JTextFields
+        campoNome.setBounds(130, 30, 200, 30);
+        campoCpf.setBounds(130, 70, 200, 30);
+        campoEndereco.setBounds(130, 110, 200, 30);
+        campotelefone.setBounds(130, 150, 200, 30);
+        campoCargo.setBounds(130, 190, 200, 30);
+
+        // Configurações da janela
+        setTitle("Cadastro de Funcionário");
+        setSize(500, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+
+        // Criação dos botões de CRUD
+        btnCadastrar = new JButton("Cadastrar");
+        btnConsultar = new JButton("Consultar");
+        btnAtualizar = new JButton("Atualizar");
+        btnExcluir = new JButton("Excluir");
+        btnCargo = new JButton("Cargos");
+
+        // Definição da posição dos botões
+        btnCargo.setBounds(345, 29, 100, 30);
+        btnCadastrar.setBounds(30, 230, 100, 30);
+        btnConsultar.setBounds(140, 230, 100, 30);
+        btnAtualizar.setBounds(250, 230, 100, 30);
+        btnExcluir.setBounds(360, 230, 100, 30);
+
+        // Configuração da JTable
+        String[] colunas = { "Nome", "CPF", "Endereço", "Telefone", "Cargo" };
+        modeloTabela = new DefaultTableModel(colunas, 0); // Modelo de tabela sem dados iniciais
+        tabelaFuncionarios = new JTable(modeloTabela);
+        JScrollPane painelTabela = new JScrollPane(tabelaFuncionarios); // Adiciona a tabela a um painel com barra de
+                                                                        // rolagem
+
+        painelTabela.setBounds(30, 270, 430, 150); // Definição da posição e tamanho da tabela
+
+        // Adiciona os componentes ao JFrame
+        add(labelNome);
+        add(campoNome);
+        add(labelCpf);
+        add(campoCpf);
+        add(labelEndereco);
+        add(campoEndereco);
+        add(labelTelefone);
+        add(campotelefone);
+        add(labelCargo);
+        add(campoCargo);
+        add(btnCadastrar);
+        add(btnConsultar);
+        add(btnAtualizar);
+        add(btnExcluir);
+        add(btnCargo);
+        add(painelTabela);
+
+        // Eventos dos Botões
+        btnCadastrar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String nome = campoNome.getText();
+                String cpf = campoCpf.getText();
+                String endereco = campoEndereco.getText();
+                String telefone = campotelefone.getText();
+                String cargo = campoCargo.getText();
+                modeloTabela.addRow(new Object[]{nome, cpf, endereco,telefone,cargo});
+
+                Funcionario funcionario = new Funcionario(nome,cpf,endereco,telefone,cargo);
+
+                listaFuncionarios.add(funcionario);
+                funcionario.exportarParaTXT(listaFuncionarios);
+
+                campoNome.setText("");
+                campoCpf.setText("");
+                campoEndereco.setText("");
+                campotelefone.setText("");
+                campoCargo.setText("");
+            }
+        });
+
+        btnConsultar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser fileChooser = new JFileChooser();
+                int resultado = fileChooser.showOpenDialog(null);
+                if(resultado == JFileChooser.APPROVE_OPTION){
+                    File arquivoSelecionado = fileChooser.getSelectedFile();
+
+                    //Limpar tabela e arrayList
+                    listaFuncionarios.clear();
+                    modeloTabela.setRowCount(0);
+
+                    try(BufferedReader dados = new BufferedReader(new FileReader(arquivoSelecionado))){
+                        String linha;
+                        while((linha = dados.readLine())!= null){
+                            String[] bdados = linha.split(";");
+                            if(bdados.length == 5){
+                                String nome = bdados[0];
+                                String cpf = bdados[1];
+                                String endereco = bdados[2];
+                                String telefone = bdados[3];
+                                String cargo = bdados[4];
+
+                                Funcionario funcionario = new Funcionario(nome, cpf, endereco, telefone, cargo);
+                                listaFuncionarios.add(funcionario);
+                                modeloTabela.addRow(new Object[]{nome, cpf, endereco, telefone, cargo});
+                            }
+                        }
+                        JOptionPane.showMessageDialog(null, "Dados Impostados com Sucesso!");
+                    } catch (IOException ex) {
+                        JOptionPane.showMessageDialog(null, "Erro ao ler o arquivo: " + ex.getMessage());
+                    }
+                }
+            }
+        });
+
+        btnAtualizar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                
+            }
+        });
+
+        btnExcluir.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                
+            }
+        });
+
+        btnCargo.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                
+            }
+        });
+
+        revalidate();
+        repaint();
+    }
+
+    public static void main(String[] args) throws Exception {
+        Main frame = new Main();
+        frame.setVisible(true);
+        Funcionario funcionario = new Funcionario();
+    }
+}
