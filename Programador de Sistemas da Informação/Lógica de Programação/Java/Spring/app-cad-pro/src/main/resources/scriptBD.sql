876954382870c895c2374386e822da5b2fc273b2
Gabriel Piske <156837794+gabrielpiske@users.noreply.github.com>
2024-10-25 14:22:18 -0300

    comentarios e ajustes

--- Programador de Sistemas da Informação/Lógica de Programação/Java/Spring/app-cad-pro/src/main/resources/scriptBD.sql
+++ Programador de Sistemas da Informação/Lógica de Programação/Java/Spring/app-cad-pro/src/main/resources/scriptBD.sql
@@ -3,12 +3,12 @@ USE productbd;
 
 CREATE TABLE `produto` (
   `id` bigint NOT NULL AUTO_INCREMENT,
-  `nome` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `descricao` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `preco` decimal(10,2) NOT NULL,
-  `quantidade` int NOT NULL,
-  `categoria` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `fornecedor` varchar(244) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `categoria` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `descricao` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `fornecedor` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `imagem` longblob,
+  `nome` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `preco` double DEFAULT NULL,
+  `quantidade` int DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
