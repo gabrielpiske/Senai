a04a9b9b9ebf25e8549529c2392abf4f1d85ad8b
Gabriel Piske <156837794+gabrielpiske@users.noreply.github.com>
2024-11-02 17:58:09 -0300

    delete

--- Programador de Sistemas da Informação/Lógica de Programação/Java/! SA - SITUAÇÃO DE APRENDIZAGEM/forn_ped/src/main/java/com/psii/forn_ped/repository/FornecedorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.psii.forn_ped.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.psii.forn_ped.model.Fornecedor;
-
-public interface FornecedorRepository extends JpaRepository<Fornecedor, Long>{
-    
-}
--- Programador de Sistemas da Informação/Lógica de Programação/Java/! SA - SITUAÇÃO DE APRENDIZAGEM/forn_ped/src/main/java/com/psii/forn_ped/repository/ProdutoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.psii.forn_ped.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.psii.forn_ped.model.Produto;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Long>{
-    
-}
