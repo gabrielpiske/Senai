7a32df76a31b9bb0d0a63eb3fa99af21d29c8a7a
Gabriel Piske <156837794+gabrielpiske@users.noreply.github.com>
2025-02-03 21:42:20 -0300

    exercicio dos parametro

--- Técnico em Desenvolvimentos de Sistemas/Desenvolvimento de Sistemas/Thread e Processos/Thread/src/App.java
+++ Técnico em Desenvolvimentos de Sistemas/Desenvolvimento de Sistemas/Thread e Processos/Thread/src/App.java
@@ -1,5 +1,30 @@
+class MinhaThread extends Thread{
+    private int tempo;
+
+    public MinhaThread(int tempo) {
+        this.tempo = tempo;
+    }
+
+    public void run(){
+        for(int i = 0; i < 5; i++){
+            System.out.println("Executando: " + Thread.currentThread().getName() + " valor do contador: " + i);
+            try {
+                Thread.sleep(tempo);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        MinhaThread thread_1 = new MinhaThread(1000);
+        thread_1.start();
+
+        MinhaThread thread_2 = new MinhaThread(1000);
+        thread_2.start();
+
+        System.out.println("Programa Finalizado");
     }
 }
