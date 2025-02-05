19207d4af3a08c08bd3803821c27536e46c0459c
Gabriel Piske <156837794+gabrielpiske@users.noreply.github.com>
2025-02-05 19:06:18 -0300

    implementando interface runnable

--- Técnico em Desenvolvimentos de Sistemas/Desenvolvimento de Sistemas/Thread e Processos/Thread Simples/src/App.java
+++ Técnico em Desenvolvimentos de Sistemas/Desenvolvimento de Sistemas/Thread e Processos/Thread Simples/src/App.java
@@ -1,13 +1,15 @@
 class MinhaThread extends Thread{
     private int tempo;
+    private String nmThread;
 
-    public MinhaThread(int tempo) {
+    public MinhaThread(int tempo, String nmThread) {
         this.tempo = tempo;
+        this.nmThread = nmThread;
     }
 
     public void run(){
         for(int i = 0; i < 5; i++){
-            System.out.println("Executando: " + Thread.currentThread().getName() + " valor do contador: " + i);
+            System.out.println("Executando: " + nmThread + " valor do contador: " + i);
             try {
                 Thread.sleep(tempo);
             } catch (InterruptedException e) {
@@ -19,10 +21,10 @@ class MinhaThread extends Thread{
 
 public class App {
     public static void main(String[] args) throws Exception {
-        MinhaThread thread_1 = new MinhaThread(1000);
+        MinhaThread thread_1 = new MinhaThread(500, "Thread 1");
         thread_1.start();
 
-        MinhaThread thread_2 = new MinhaThread(1000);
+        MinhaThread thread_2 = new MinhaThread(3000, "Thread 2");
         thread_2.start();
 
         System.out.println("Programa Finalizado");
