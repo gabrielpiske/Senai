dab8c9496f79482e2f42864d1dc20c83351a3d27
Gabriel Piske <156837794+gabrielpiske@users.noreply.github.com>
2025-03-04 21:40:23 -0300

    validador cpf

---
+++ Técnico em Desenvolvimentos de Sistemas/Testes de Sistemas/Teste-CPF/src/App.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        Verificador verificador = new Verificador();
+        String cpf = JOptionPane.showInputDialog("Informe o CPF a ser validado (Somente Números): ");
+        if (verificador.verificarCpf(cpf)) {
+            JOptionPane.showMessageDialog(null, "CPF válidado", "Validação", JOptionPane.INFORMATION_MESSAGE);
+        } else {
+            JOptionPane.showMessageDialog(null, "CPF invalido", "Erro", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+}
---
+++ Técnico em Desenvolvimentos de Sistemas/Testes de Sistemas/Validador-CPF/src/test/java/com/piske/validador/cpf/VerificadorTest.java
@@ -0,0 +1,28 @@
+package com.piske.validador.cpf;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author gabriel_piske
+ */
+public class VerificadorTest {
+    
+    public VerificadorTest() {
+    }
+
+    @Test
+    public void FormatoCpf() {
+
+    }
+    
+    @Test
+    public void validarVerificador(){
+
+    }
+
+    @Test void calcularDigitoVerificador(){
+        
+    }
+}
