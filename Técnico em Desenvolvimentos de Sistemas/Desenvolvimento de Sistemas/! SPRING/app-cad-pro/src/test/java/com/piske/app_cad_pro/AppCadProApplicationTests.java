034f27388cbcbbcae21924ec2b4a7cd18ed45f4a
Gabriel Piske <156837794+gabrielpiske@users.noreply.github.com>
2025-03-17 19:15:53 -0300

    create project cad pro

---
+++ Técnico em Desenvolvimentos de Sistemas/Desenvolvimento de Sistemas/! SPRING/app-cad-pro/src/test/java/com/piske/app_cad_pro/AppCadProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piske.app_cad_pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCadProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
